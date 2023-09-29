1ca3c5f5815b87b09caec32cbe9793d4d4629c5e
HanLeqi0117 <k895657@kansai-u.ac.jp>
2023-09-29 12:32:08 +0900

    move the files from launch/include into launch and remove the include

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 # Packageの名前
 package_name = 'whill_navi2'
+SHARE_DIR = os.path.join('share', package_name)
 
 # このPackageをビルドするメソッド
 # 引数1：Packageの名前
-# 引数2：依存するPackageを探す。しかし「test」に関するPackageを除外する。
-# 引数3：インストールする際に、データファイルの保存先。例：config/param, launchなど
-# 引数4：インストールする際に、依存するライブラリー
-# 引数5：zipアーカイブ形式を有効にするかどうか。アーカイブとは、URL：https://wa3.i-3-i.info/word1247.html
-# 引数6~9：開発者情報やライセンス情報
-# 引数10：テスト方式
-# 引数11：インストールするスクリプトの居場所。例：実行ファイルの名前 = パッケージ名.スクリプト名(拡張子なし).main(メソッド名)
+# 引数2：バージョン
+# 引数3：依存するPackageを探す。しかし「test」に関するPackageを除外する。
+# 引数4：インストールファイルのアドレスとインストール先を指定する。Tupleの1つ目の要素はインストール先を指定し、2つ目の要素の型はリストであり、インストールファイルのアドレスを指定する。
+# 引数5：インストールする際に、依存するライブラリー
+# 引数6：zipアーカイブ形式を有効にするかどうか。アーカイブとは、URL：https://wa3.i-3-i.info/word1247.html
+# 引数7~10：開発者情報やライセンス情報
+# 引数11：テスト方式
+# 引数12：インストールするスクリプトの居場所。例：実行ファイルの名前 = パッケージ名.スクリプト名(拡張子なし).main(メソッド名)
 
 setup(
     name=package_name,
@@ -21,6 +25,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join(SHARE_DIR, 'launch'), glob(os.path.join('launch', '*launch.py'), recursive=True))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
